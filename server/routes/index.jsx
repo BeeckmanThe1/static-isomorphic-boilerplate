b4bdda920d73be144950c37dac68800a697a20c8
Thomas Opdebeeck <thomas.opdebeeck@realdolmen.com>
2021-05-07 23:25:06 +0200

    Small clean up

--- server/routes/index.jsx
+++ server/routes/index.jsx
@@ -1,11 +1,6 @@
-import {renderToString} from "react-dom/server";
-import FirstPage from "../../hybrid/pages/page-1/FirstPage.jsx";
 import React from 'react';
 import {getPageHtml} from "../htmlTemplating/templateProvider";
-import {WRAPPER_ID as WRAPPER_ID_1} from '../../hybrid/pages/page-1/FirstPage.jsx';
-import SecondPage, {WRAPPER_ID as WRAPPER_ID_2} from '../../hybrid/pages/page-2/SecondPage.jsx';
 import WEBSITE_SETUP from "../WEBSITE_SETUP";
-import PageToJsxMapper from "../../hybrid/utils/PageToJsxMapper/PageToJsxMapper";
 
 const init = async router => {
 
